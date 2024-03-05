6b5fbebea80fe7e36e42612bc8a1511464bfe108
anjaligupta <er.anjaliguptasrgi@gmail.com>
2024-03-05 12:52:52 +0530

    providesolutionforquestion3

---
+++ question3pythonprogram.py
@@ -0,0 +1,15 @@
+# Python program to check if year is a leap year or not
+
+year = 5000
+
+
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year for ".format(year))
+
+
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year for ".format(year))
+
+
+else:
+    print("{0} is not a leap year for ".format(year))
